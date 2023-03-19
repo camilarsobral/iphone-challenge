e64fcb20180adfb3f350d4ff0c2ededb8981f0d9
camilarsobral <camilarsobral@hotmail.com>
2023-03-18 22:03:45 -0300

    1st commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function switchColour(colour) {
+    let circulo = document.querySelector('.circulo')
+    circulo.style.background = colour
+    console.log(circulo)
+}
+
+function switchImage(image) {
+    let imgiPhone = document.querySelector('.iphones')
+    imgiPhone.src = image
+}
